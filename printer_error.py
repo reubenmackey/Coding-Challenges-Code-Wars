f7d4036c6b334d255abe4db312a8442a9ac1da2e
Reuben Mackey <ReubenMackey@Reubens-MacBook.local>
2019-02-10 21:16:23 +1300

    Added Printer_Error_Challenge

---
+++ printer_error.py
@@ -0,0 +1,243 @@
+# In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+# The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+# Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm.
+# You have to write a function printer_error which given a string will output the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+# The string has a length greater or equal to one and contains only letters from ato z.
+# #Examples:
+# s="aaabbbbhaijjjm"
+# error_printer(s) => "0/14"
+#
+# s="aaaxbbbbyyhwawiwjjjwwm"
+# error_printer(s) => "8/22"
+
+import string
+
+def printer_error(s):
+    alphabet = string.ascii_lowercase
+    valid_letters = alphabet[0:13]
+    good = 0
+    bad = 0
+    for letter in s:
+        if letter not in valid_letters:
+            bad += 1
+        else:
+            good += 1
+    return "{}/{}".format(str(bad),str(good+bad))
+
+print(printer_error('aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz'))
+
+##OUTPUT
+
+#Result
+#"3/56"
+
+# Time: 846ms Passed: 203 Failed: 0
+# Test Results:
+#  printer_error
+#  Basic tests
+# Test Passed
+# Test Passed
+# Test Passed
+#  Log
+# Random tests ******************
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# Test Passed
+# You have passed all of the tests! :)
